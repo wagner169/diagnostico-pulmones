c965482057a9bab0c1701d4b1efee3fd82abc499
Wagner Moreno <wamoreno@uees.edu.ec>
2025-07-30 11:17:10 -0500

    🔧 Cambié el puerto a 10000 para coincidir con Render

--- api/main.py
+++ api/main.py
@@ -78,4 +78,4 @@ async def predict(file: UploadFile = File(...)):
 
 # Para correr localmente: uvicorn api.main:app --reload
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
